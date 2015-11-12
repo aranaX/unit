36b5ab8ec228507984e05c31de1029f6795d20c1
Wences Arana <aranax@debian.org.gt>
2015-11-12 00:19:33 -0600

    Add controllers and unit test classes

---
+++ example/farmacia/src/test/java/org/oms/farmacia/controllers/BookmarkRestControllerTest.java
@@ -0,0 +1,154 @@
+package org.oms.farmacia.controllers;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+import org.oms.farmacia.Application;
+import org.oms.farmacia.model.Account;
+import org.oms.farmacia.model.AccountRepository;
+import org.oms.farmacia.model.Bookmark;
+import org.oms.farmacia.model.BookmarkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * @author Josh Long
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class BookmarkRestControllerTest {
+
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+
+    private String userName = "bdussault";
+
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    private Account account;
+
+    private List<Bookmark> bookmarkList = new ArrayList<>();
+
+    @Autowired
+    BookmarkRestController bookmarkRestController;
+    
+    @Mock
+    private BookmarkRepository bookmarkRepository;
+    @Mock
+    private WebApplicationContext webApplicationContext;
+    @Mock
+    private AccountRepository accountRepository;
+    
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+/*
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(
+                hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().get();
+
+        Assert.assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }*/
+
+    @Before
+    public void setup() throws Exception {
+        
+        }
+
+    @Test(expected = UserNotFoundException.class)
+    public void userNotFound() {        
+        Bookmark bookmark = new Bookmark(account, "", "");
+        bookmarkList.add(bookmark);
+        ResponseEntity response;
+        when(accountRepository.findByUsername( userName )).thenReturn( Optional.empty() );
+                
+        bookmarkRestController.add( "george", bookmark);                
+    }
+
+//    @Test
+//    public void readSingleBookmark() throws Exception {
+//        Bookmark bookmark = new Bookmark(account, "", "");
+//        bookmarkList.add(bookmark);
+//        ResponseEntity response;
+//        when(accountRepository.findByUsername( userName )).thenReturn( Optional.empty() );
+//        
+//        Collection<Bookmark> listBookmark = bookmarkRestController.readBookmarks( userName );
+//        
+//        assertEquals( listBookmark.contains( this), account);
+//
+//        
+//        mockMvc.perform(get("/" + userName + "/bookmarks/"
+//                + this.bookmarkList.get(0).getId()))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentType(contentType))
+//                .andExpect(jsonPath("$.id", is(this.bookmarkList.get(0).getId().intValue())))
+//                .andExpect(jsonPath("$.uri", is("http://bookmark.com/1/" + userName)))
+//                .andExpect(jsonPath("$.description", is("A description")));
+//    }
+
+//    @Test
+//    public void readBookmarks() throws Exception {
+//        mockMvc.perform(get("/" + userName + "/bookmarks"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentType(contentType))
+//                .andExpect(jsonPath("$", hasSize(2)))
+//                .andExpect(jsonPath("$[0].id", is(this.bookmarkList.get(0).getId().intValue())))
+//                .andExpect(jsonPath("$[0].uri", is("http://bookmark.com/1/" + userName)))
+//                .andExpect(jsonPath("$[0].description", is("A description")))
+//                .andExpect(jsonPath("$[1].id", is(this.bookmarkList.get(1).getId().intValue())))
+//                .andExpect(jsonPath("$[1].uri", is("http://bookmark.com/2/" + userName)))
+//                .andExpect(jsonPath("$[1].description", is("A description")));
+//    }
+//
+//    @Test
+//    public void createBookmark() throws Exception {
+//        String bookmarkJson = json(new Bookmark(
+//                this.account, "http://spring.io", "a bookmark to the best resource for Spring news and information"));
+//        this.mockMvc.perform(post("/" + userName + "/bookmarks")
+//                .contentType(contentType)
+//                .content(bookmarkJson))
+//                .andExpect(status().isCreated());
+//    }
+//
+//    protected String json(Object o) throws IOException {
+//        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+//        this.mappingJackson2HttpMessageConverter.write(
+//                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+//        return mockHttpOutputMessage.getBodyAsString();
+//    }
+}
